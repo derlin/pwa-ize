3ffccde5c711743a9c33ab942f130967def2921d
Lucy Linder <lucy.derlin@gmail.com>
2021-10-28 20:00:05 +0200

    Update social-preview

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,7 +10,8 @@
           PWA-ize let you transform any website into a
           <span class="accent-text">P</span>rogressive <span class="accent-text">W</span>eb
           <span class="accent-text">A</span>pplication. A PWA can be added to your homescreen and
-          will open on a separate (and dedicated) browser window, thus feeling like a regular app.
+          will open on a separate (and dedicated) browser window, thus feeling more like a regular
+          app.
         </p>
         <p>
           You can either install a particular website as PWA using this page, or use the
